1fe626b05eac9b3596c66d6b126adf5dc13b9921
feiyyx <feiyy2008@qq.com>
2022-05-29 20:02:05 +0800

    feat: init program

---
+++ backgroud.js
@@ -0,0 +1,9 @@
+console.log('test');
+document.querySelector('.diy-option').addEventListener('click', function() {
+  // if (chrome.runtime.openOptionsPage) {
+  //   chrome.runtime.openOptionsPage();
+  // } else {
+    console.log('test');
+    window.open(chrome.runtime.getURL('../html/options.html'));
+  // }
+});
